5a6399755d5030ed2d8ab11f06e2d82990aa3998
brunoczim <brunoczim@users.noreply.github.com>
2021-11-21 04:14:42 +0000

    deploy: 543ba50802027272af0a106ad15891ccc399c0c8

---
+++ implementors/core/ops/index/trait.Index.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["langsrc"] = [{"text":"impl&lt;I&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.56.1/core/ops/index/trait.Index.html\" title=\"trait core::ops::index::Index\">Index</a>&lt;I&gt; for <a class=\"struct\" href=\"langsrc/struct.Source.html\" title=\"struct langsrc::Source\">Source</a> <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;I: <a class=\"trait\" href=\"langsrc/trait.SourceIndex.html\" title=\"trait langsrc::SourceIndex\">SourceIndex</a>,&nbsp;</span>","synthetic":false,"types":["langsrc::Source"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ source-files.js
@@ -0,0 +1,4 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["langsrc"] = {"name":"","files":["indexing.rs","lib.rs","loc.rs","reader.rs","span.rs"]};
+sourcesIndex["unicode_segmentation"] = {"name":"","files":["grapheme.rs","lib.rs","sentence.rs","tables.rs","word.rs"]};
+createSourceSidebar();
