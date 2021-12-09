0dedeaca315a0fe1347452a1fa6ec224951f92e7
brunoczim <brunoczim@users.noreply.github.com>
2021-12-09 15:06:52 +0000

    deploy: a927d2a488fe845e12e015a34f02514f48bc5874

--- bittongue/sidebar-items.js
+++ bittongue/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"mod":[["diagnostic",""],["source","This module provides utilities to handle source code of a programming language."]]});
+initSidebarItems({"mod":[["diagnostic","This module exports error-related utilities for parse/compile-time of a programming language."],["source","This module provides utilities to handle source code of a programming language."]]});
