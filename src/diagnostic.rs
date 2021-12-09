a927d2a488fe845e12e015a34f02514f48bc5874
brunoczim <brunoczim@gmail.com>
2021-12-09 12:04:07 -0300

    doc for errors

--- src/diagnostic.rs
+++ src/diagnostic.rs
@@ -1,24 +1,37 @@
+//! This module exports error-related utilities for parse/compile-time of a
+//! programming language.
+
 use crate::source::Span;
 use std::{
     any::{Any, TypeId},
     fmt,
     rc::Rc,
+    slice,
     sync::Arc,
     vec,
 };
 
+/// Level of a given diagnostic.
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub enum Level {
+    /// This is just a note, easily ignored.
     Note,
+    /// This is a warning, should be read carefully.
     Warning,
+    /// A hard error, cannot be ignored at all.
     Error,
 }
 
+/// Diagnostic: a problem or note found in a source code.
 pub trait Diagnostic: fmt::Display + fmt::Debug + Any {
+    /// Severity level of this diagnostic.
     fn level(&self) -> Level;
 
+    /// Yields the primary span related to the diagnostic, if any.
     fn primary_span(&self) -> Option<Span>;
 
+    /// Yields an iterator over secondary spans related to the diagnostic, if
+    /// any.
     fn secondary_spans<'this>(
         &'this self,
     ) -> Option<Box<dyn Iterator<Item = Span> + Send + Sync + 'this>> {
@@ -147,22 +160,58 @@ impl_downcast! { dyn Diagnostic + Send }
 impl_downcast! { dyn Diagnostic + Sync }
 impl_downcast! { dyn Diagnostic + Send + Sync }
 
+/// A collection of diagnostics. Generic on the type of diagnostics, but
+/// intended for trait objects, such as `dyn Diagnostic` or `dyn Diagnostic +
+/// Send + Sync` (the last one is the default).
 #[derive(Debug)]
-pub struct Diagnostics<D>
+pub struct Diagnostics<D = dyn Diagnostic + Send + Sync>
 where
     D: Diagnostic + ?Sized,
 {
     elements: Vec<Box<D>>,
 }
 
+impl<D> Default for Diagnostics<D>
+where
+    D: Diagnostic + ?Sized,
+{
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
 impl<D> Diagnostics<D>
 where
     D: Diagnostic + ?Sized,
 {
+    /// Creates an empty collection of diagnostics.
     pub fn new() -> Self {
         Self { elements: Vec::new() }
     }
 
+    /// Returns whether the source code status is OK (no hard errors) according
+    /// to this collection of diagnostics.
+    pub fn is_ok(&self) -> bool {
+        self.max_level().map_or(true, |level| level < Level::Error)
+    }
+
+    /// Returns whether the source code status is NOT OK (there are hard errors)
+    /// according to this collection of diagnostics.
+    pub fn is_err(&self) -> bool {
+        self.max_level().map_or(false, |level| level >= Level::Error)
+    }
+
+    /// Returns the maximum level among the diagnostics in this collection.
+    pub fn max_level(&self) -> Option<Level> {
+        self.iter().map(Diagnostic::level).max()
+    }
+
+    /// Creates an iterator over references of diagnostics.
+    pub fn iter(&self) -> Iter<D> {
+        self.into_iter()
+    }
+
+    /// Raises a new diagnostic and saves it in this collection.
     pub fn raise<T>(&mut self, diagnostic: T)
     where
         Box<T>: Into<Box<D>>,
@@ -171,11 +220,13 @@ where
     }
 }
 
+/// Owned iterator over diagnostics of the [`Diagnostics`] collection.
 #[derive(Debug)]
 pub struct IntoIter<D>
 where
     D: Diagnostic + ?Sized,
 {
+    /// Inner owned vector iterator.
     inner: vec::IntoIter<Box<D>>,
 }
 
@@ -187,5 +238,50 @@ where
 
     fn next(&mut self) -> Option<Self::Item> {
         self.inner.next()
+    }
+}
+
+impl<D> IntoIterator for Diagnostics<D>
+where
+    D: Diagnostic + ?Sized,
+{
+    type Item = Box<D>;
+    type IntoIter = IntoIter<D>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        IntoIter { inner: self.elements.into_iter() }
+    }
+}
+
+/// Borrowed iterator over diagnostics of the [`Diagnostics`] collection.
+#[derive(Debug)]
+pub struct Iter<'diag, D>
+where
+    D: Diagnostic + ?Sized,
+{
+    /// Iterator over slice references.
+    inner: slice::Iter<'diag, Box<D>>,
+}
+
+impl<'diag, D> Iterator for Iter<'diag, D>
+where
+    D: Diagnostic + ?Sized,
+{
+    type Item = &'diag D;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.inner.next().map(|boxed| boxed.as_ref())
+    }
+}
+
+impl<'diag, D> IntoIterator for &'diag Diagnostics<D>
+where
+    D: Diagnostic + ?Sized,
+{
+    type Item = &'diag D;
+    type IntoIter = Iter<'diag, D>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        Iter { inner: self.elements.iter() }
     }
 }
